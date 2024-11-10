e90433e1efc0e609828d99519427d670e0ad6548
Joseph2006t <laizatellojoseph@gmail.com>
2024-11-10 18:30:07 -0500

    commit de prueba

--- Primer Repositorio/Programa1.js
+++ Primer Repositorio/Programa1.js
@@ -1,3 +1,9 @@
 console.log("Hello world");
 console.log("Logre crear un commit desde visual estudio code");
 console.log("Prueba otra vez");
+let numero = 10;
+if(numero%2==0){
+    console.log("El número es par");
+}else{
+    console.log("El número es impar");
+}
